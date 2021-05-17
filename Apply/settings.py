5c2219ee93439efb489aed59e2f36f03e25ea722
gyudong <esatar658@gmail.com>
2021-05-17 11:41:39 +0900

    v

--- Apply/settings.py
+++ Apply/settings.py
@@ -75,27 +75,27 @@ WSGI_APPLICATION = 'Apply.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-import dj_database_url
-
 DATABASES = {
-    'default' : {
-        'ENGINE : django.db.backends.postgresql_psycopg2',
-        'NAME' : '',
-        'USER' : '',
-        'PASSWORD' : '',
-        'HOST' : '',
-        'PORT' : '',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['dafault'].update(db_from_env)
+
+# import dj_database_url
+
+# DATABASES = {
+#     'default' : {
+#         'ENGINE : django.db.backends.postgresql_psycopg2',
+#         'NAME' : '',
+#         'USER' : '',
+#         'PASSWORD' : '',
+#         'HOST' : '',
+#         'PORT' : '',
+#     }
+# }
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['dafault'].update(db_from_env)
 
 
 # Password validation
